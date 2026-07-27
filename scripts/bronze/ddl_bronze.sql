db80784bf34d5bb64b4285ddfbb47a21f2a39205
Martin Sunga <140482688+martinsunga@users.noreply.github.com>
2026-07-27 17:41:29 +0800

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -44,7 +44,7 @@ CREATE TABLE bronze.crm_prd_info (
 	prd_id			INT,
 	prd_key			NVARCHAR(50),
 	prd_nm			NVARCHAR(50),
-	prd_cost		INT,
+	prd_cost		DECIMAL(10,2),
 	prd_line		NVARCHAR(50),
 	prd_start_dt	DATE,
 	prd_end_dt		DATE
