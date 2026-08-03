0ce7e0180e741982eae97ab10f2e54935c82e82b
Martin Sunga <140482688+martinsunga@users.noreply.github.com>
2026-08-03 13:13:54 +0800

    Align SQL table column definitions for consistency

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -30,14 +30,14 @@ Tables created:
 DROP TABLE IF EXISTS silver.crm_cust_info;
 GO
 CREATE TABLE silver.crm_cust_info (
-	cst_id			INT,
-	cst_key			NVARCHAR(50),
+	cst_id				INT,
+	cst_key				NVARCHAR(50),
 	cst_firstname		NVARCHAR(50),
 	cst_lastname		NVARCHAR(50),
 	cst_marital_status	NVARCHAR(50),
-	cst_gndr		NVARCHAR(50),
+	cst_gndr			NVARCHAR(50),
 	cst_create_date		DATE,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+	dwh_create_date 	DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -51,7 +51,7 @@ CREATE TABLE silver.crm_prd_info (
 	prd_nm			NVARCHAR(50),
 	prd_cost		DECIMAL(10,2),
 	prd_line		NVARCHAR(50),
-	prd_start_dt		DATE,
+	prd_start_dt	DATE,
 	prd_end_dt		DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
@@ -64,11 +64,11 @@ CREATE TABLE silver.crm_sales_details (
 	sls_ord_num		NVARCHAR(50),
 	sls_prd_key		NVARCHAR(50),
 	sls_cust_id		INT,
-	sls_order_dt		DATE,
+	sls_order_dt	DATE,
 	sls_ship_dt		DATE,
 	sls_due_dt		DATE,
 	sls_sales		DECIMAL(10,2),
-	sls_quantity		INT,
+	sls_quantity	INT,
 	sls_price		DECIMAL(10,2),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
@@ -78,9 +78,9 @@ GO
 DROP TABLE IF EXISTS silver.erp_cust_az12;
 GO
 CREATE TABLE silver.erp_cust_az12 (
-	cid		NVARCHAR(50),
-	bdate		DATE,
-	gen		NVARCHAR(50),
+	cid				NVARCHAR(50),
+	bdate			DATE,
+	gen				NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -89,8 +89,8 @@ GO
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 GO
 CREATE TABLE silver.erp_loc_a101 (
-	cid		NVARCHAR(50),
-	cntry	NVARCHAR(50),
+	cid				NVARCHAR(50),
+	cntry			NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -99,10 +99,10 @@ GO
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
 GO
 CREATE TABLE silver.erp_px_cat_g1v2 (
-	id			NVARCHAR(50),
-	cat			NVARCHAR(50),
-	subcat		NVARCHAR(50),
-	maintenance NVARCHAR(50),
+	id				NVARCHAR(50),
+	cat				NVARCHAR(50),
+	subcat			NVARCHAR(50),
+	maintenance 	NVARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
